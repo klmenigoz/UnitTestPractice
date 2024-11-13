ae6d763444056327d4f5960fa09a6a8f878ed642
Kent Menigoz <klmenigoz@csuchico.edu>
2024-11-13 12:02:05 -0800

    x12 Unit Test Practice

--- Password.cpp
+++ Password.cpp
@@ -10,7 +10,8 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
-  int repetition = 1;
+  // could add an if to check length being 0, and if thats true then return 0 immediately to bypass the bug
+  int repetition = 0;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
     repetition++;
@@ -19,11 +20,26 @@ int Password::count_leading_characters(string phrase){
   return repetition;
 }
 
-
 /*
   receives a string and returns whether it has both at least one upper-case
   letter and at least one lower-case letter
 */
-bool Password::has_mixed_case(string pass){
+bool Password::has_mixed_case(string pass) {
+  bool hasUpper = false;
+  bool hasLower = false;
+
+  for (int index = 0; index < pass.length(); index++) {
+    if (isupper(pass[index])) {
+      hasUpper = true;
+    } else if (islower(pass[index])) {
+      hasLower = true;
+    }
+    // If both conditions are met, no need to check further
+    if (hasUpper && hasLower) {
+      return true;
+    }
+  }
+
+  // Return true only if both conditions were met
   return false;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,81 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+/////////////////////////////// COUNT LEADING //////////////////////////////////////////////
+TEST(PasswordTest, single_letter_password)	// single_letter password is the name of this particular unit test CASE	
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
+	int actual = my_password.count_leading_characters("Z");	// "happy" test, will pass
+	ASSERT_EQ(1, actual);	// asserting that actual value is 1
+}
+
+TEST(PasswordTest, null_input)	// the name of this second test case is
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");	// "sad" test, will not pass
+	ASSERT_EQ(0, actual);	// asserting that actual value is 1
+}
+
+TEST(PasswordTest, three_letter_password)	// the name of this second test case is three letter password, asserting 1 for a not showing up a second time
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aAa_nonrepeating");	// "happy" test, will pass
+	ASSERT_EQ(1, actual);	// asserting that actual is 2 for the repeated 'a'
+}
+
+TEST(PasswordTest, three_letter_password_repeated)	// the name of this second test case is three letter password, asserting 1 for a not showing up a second time
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaa");	// 
+	ASSERT_EQ(1, actual);	// asserting that actual is 3 for the repeated 'a'
+}
+
+/////////////////////////////// MIXED CASE //////////////////////////////////////////////
+TEST(PasswordTest, good_mixed_case)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("Zz");	// expected usage, is mixed
 	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, bad_mixed_case)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("ZZ");	// should not pass, not mixed case
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, bad_mixed_case_2)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("zz");	// should not pass, not mixed case
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, has_special)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("!?wTF");	// should pass, has upper and lower but spechal chars first
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, has_special_and_space)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("I am not(?) okay");	// should pass, has capitalized I
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, null_password)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("");	// should not pass, not mixed case (null password)
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, number)	
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("1234");	// should not pass, not mixed, no chars even
+	ASSERT_EQ(0, actual);
 }
